a23007992e83e8c99a897346579f8c1060a3be8d
Emanuel <emanuel.n.nagima@gmail.com>
2025-03-22 10:51:02 -0300

    operações

--- valores.py
+++ valores.py
@@ -5,11 +5,13 @@ valor_pi=input('Digite o novo valor de valor_pi')
 print(valor_pi)
 
 
-nome=input('digite seu nome')
+nome=str(input('digite seu nome')) #texto
 idade=int(input('digite sua idade')) #21
 peso=float(input('digite seu peso')) #68.5
+altura=float(input('digite sua altura')) #1.75
 
 print(nome)
 print(idade)
 print(peso)
 print(type(peso))
+print(f'Você é o {nome}e tem {idade} de idade? Altura de {altura} e peso de {peso}kg, correto?')
